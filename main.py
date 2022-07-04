c919c9d44f54436c2d8c2cf610506931b0b6272e
Jonas Villacis <jonasvillacis@yahoo.com>
2022-07-04 18:40:52 -0500

    Add a new methods in main module

---
+++ main.py
@@ -0,0 +1,17 @@
+
+class Main:
+    
+    def __init__(self):
+        pass
+
+    def archivoRandom(self,nombre,numPalabras):
+        pass
+
+    def buscarPalabra(palabra)-> int:
+        return "aaaaa"
+
+   
+main = Main()
+
+answer =main.buscarPalabra()
+print (answer)
